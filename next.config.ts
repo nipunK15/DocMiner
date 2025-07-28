d30e268e106bfc832d2664568ff52b19fb33187b
harshikabansal <157413622+harshikabansal@users.noreply.github.com>
2025-07-28 23:34:44 +0530

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse'],
+  },
+};
+
+export default nextConfig;
